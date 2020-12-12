0c4fb10241bfb008c54c4dbecc2f4b910e6d0633
Alex Emanuel <alexprojetos27>
2020-12-12 14:57:20 -0300

    manage command line arguments with argparse

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
-import sys
+import argparse 
 
 from Nqueens import NQueens
 from search_algorithms import breadth_first_search as BFS
+from search_algorithms import depth_first_search as DFS
 from utils import print_queens_board_positions
 
 
 if __name__ == '__main__':
-    #Number of queens and board size (NxN)
-    N = int(sys.argv[1])
-    n_queens = NQueens(N)
 
-    queens_positions = BFS(n_queens)
+    parser = argparse.ArgumentParser(description = "Apply a search algorithm to solve the eight queens problem")
+
+    parser.add_argument("--search_algorithm",
+                        help = "Choose between the search algorithms: BFS (breadth_first_search) and DFS (depth_first_search) (default: DFS)",
+                        default = "DFS",
+                        choices = ["DFS", "BFS"])
+
+    parser.add_argument("--queens_number",
+                        help = "Number of queens on the board (default: 8)",
+                        type = int,
+                        default = 8)
+
+    args = parser.parse_args()
+
+    if args.search_algorithm == "DFS":
+        search_algorithm = DFS
+    else:
+        search_algorithm = BFS
+
+    n_queens = NQueens(args.queens_number)
+    queens_positions = search_algorithm(n_queens)
 
     if n_queens.goal_test(queens_positions):
         print(f"One solution was found: {queens_positions}")
         print_queens_board_positions(queens_positions)
 
     else:
-        print("Could not solve the problem")
+        print("None solution was found")
         
 
